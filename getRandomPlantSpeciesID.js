e0221fb46911c00252e6f156bf983c389956d4bf
Filip Štropin <filip@iskratrade.hr>
2022-04-01 10:49:21 +0200

    Issue #12 Middleware - kreirana funkcija za nasumičan odabir id-a iz biljne vrste iz JSON formata zapisa u kojem se nalazi popis svih biljnih vrsta.

--- getRandomPlantSpeciesID.js
+++ getRandomPlantSpeciesID.js
@@ -1,7 +1,7 @@
 
 /*getRandomPlantSpeciesID() 
  *funkcija uzima prethodno kreiran json dokument koji sadrzi sve biljne vrste.
- *koristeci metodu marth.random, uzima se nasumicni objekt iz "data" kljuca
+ *koristeci metodu marth.random, uzima se nasumicna "id" vrijednost iz "data" kljuca
  *kao rezultat funkcije vraca se id vrijednost*/
 
 function getRandomPlantSpeciesID()

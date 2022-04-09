2ab18b9874788ed277a54f4b34886deea19ab248
Bojan159 <bojanklipa@gmail.com>
2022-04-09 20:18:32 +0200

    Dodana Datoteka Pitanje.html i azurirana CheckAnswer.js

--- CheckAnswer.js
+++ CheckAnswer.js
@@ -1,8 +1,23 @@
-function checkAnswer(){
-    return true;
+let question ={
+    title: 'Pitanje?',
+    answer: ['Odgovor1','Odgovor2','Odgovor3','Odgovor4'],
+    correctAnswer: 1
+};
+
+function showQuestion(q){
+    let titleDiv = document.getElementById('title');
+    titleDiv.textContent=q.title;
+    let alts= document.querySelectorAll('.alternative');
+    alts.forEach(function(element, index){
+        element.textContent=q.answer[index];
+        element.addEventListener('click', function(){
+            if(q.correctAnswer==index) {
+                alert("Tocno!");
+            }else{
+                alert("Netocno!");
+            }
+        })
+    })
 }
-if (checkAnswer()){
-    console.log('Vas odgovor je tocan');
-}else{
-    console.log('Vas odgovor je netocan');
-}
+
+showQuestion(question);

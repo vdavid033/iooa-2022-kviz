c8e9fdfcb912c8e55b0183f64e3a3b6a7e17110b
sbrozan <stefano.ecomputing@gmail.com>
2022-04-13 15:57:37 +0200

    Funckija za nekoliko nasumičnih biljnih porodica

--- getRandomBotanicalPlantID.js
+++ getRandomBotanicalPlantID.js
@@ -4,10 +4,12 @@ function getRandomBotanicalPlantID (){
     var botanicList = new Array();
 
     for(var i=0; i<4; i++){
-
+    var botanicObject = {}
     var index = Math.round(Math.random() * (json["data"].length - 1));
-
-    botanic.push(json["data"][index]);
+    botanicObject["id"] = json["data"][index].id;
+    botanicObject["croatian_name"] = json["data"][index].croatian_name;
+    botanicObject["latin_name"] = json["data"][index].latin_name;
+    botanicList.push(botanicObject);
 } 
 
     console.log(botanicList);

ae7a5a18d4bb831f5bb81ce2fe4036f3adf8011c
unknown <gulija.nino@gmail.com>
2022-04-18 16:06:56 +0200

    Dodavanje home button (više primjera, link koji je trenutno postavljen za home http://agro.veleri.hr/biljne-vrste/ )

---
+++ frontend/QBtn.vue
@@ -0,0 +1,73 @@
+// Primjeri za home screen button 
+<template>
+
+
+
+  <div class="q-pa-md q-gutter-sm">
+     <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+     label="Početna stranica"  
+     color="green" 
+     icon="home" 
+     />
+
+
+      <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+      label="Početna stranica" 
+      outline color="green" 
+      icon="home" 
+      />
+     
+      <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+      color="green" 
+      icon="home" 
+      />
+
+      <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+      outline color="green" 
+      icon="home" 
+      />
+
+      <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+      outline color="green" 
+      icon="eco" 
+      />
+   
+      <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+      color="green" 
+      icon="eco" 
+      />
+
+      <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+      label="Početna stranica" 
+      outline color="green" 
+      icon="eco" 
+      />
+
+      <q-btn href="http://agro.veleri.hr/biljne-vrste/" 
+      label="Početna stranica" 
+      color="green" 
+      icon="eco" 
+      /> 
+  
+  </div>
+
+</template>
+
+<script>
+export default {
+  setup () {
+    function linkClick (e, go) {
+      e.preventDefault() // we choose when we navigate
+
+      // console.log('triggering navigation in 2s')
+      setTimeout(() => {
+        // console.log('navigating as promised 2s ago')
+        go()
+      }, 2000)
+    }
+
+    return { linkClick }
+  }
+}
+</script>
+

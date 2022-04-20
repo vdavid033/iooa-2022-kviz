20420e41829e96c545ead2f11068f2347e9979e5
Filip <vasiljevic.filip@gmail.com>
2022-04-20 18:35:56 +0200

    razlicite boje radio i popup

--- frontend/IndexPage.vue
+++ frontend/IndexPage.vue
@@ -12,20 +12,65 @@
   <div class="q-pa-md">
     <div class="q-col-gutter-md row items-start">
       <div class="col-4">
-        5. Pitanje - Kojoj botaničkoj porodici pripada (naziv biljke)?
+        5. Pitanje - Kojoj botaničkoj porodici pripada ljubičica?
         <q-img
-          src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg"
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Violet.JPG"
           :ratio="16 / 9"
         />
       </div>
     </div>
   </div>
   <div class="q-pa-md">
-    <q-option-group :options="options" type="radio" v-model="group" />
+     <q-item tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-radio v-model="color" val="violaceae" color="green" @click = "selection = true"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Violaceae</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-radio v-model="color" val="amaryllidaceae" color="red" @click = "selection = false"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Amaryllidaceae</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-radio v-model="color" val="asteraceae" color="red" @click = "selection = false"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Asteraceae</q-item-label>
+        </q-item-section>
+      </q-item>
   </div>
   <div class="q-pa-md q-gutter-sm">
-    <q-btn color="white" text-color="black" label="Prihvati odgovor" />
+    <q-btn color="white" text-color="black" label="Prihvati odgovor" @click="confirm = true"/>
     <q-btn color="white" text-color="black" label="Završi i predaj" />
+  </div>
+  <div>
+    <q-dialog v-model="confirm" persistent>
+      <q-card>
+        <q-card-section class="q-pt-none">
+          Odabrali ste <strong> {{ color }}</strong>
+        </q-card-section>
+        <q-card-section>
+          Točan odgovor je violaceae
+        </q-card-section>
+        <q-card-section>
+          Vaš odgovor je <strong> {{selection}} </strong>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup></q-btn>
+        </q-card-actions>
+
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -35,14 +80,11 @@ import { ref } from "vue";
 export default {
   setup() {
     return {
-      group: ref(null),
+      confirm: ref(false),
+      color: ref("cyan"),
+      selection: ref(false),
 
-      options: [
-        { label: "Odgovor 1", value: "imebiljke1", color: "green" },
-        { label: "Odgovor 2", value: "imebiljke2", color: "red" },
-        { label: "Odgovor 3", value: "imebiljke3" },
-      ],
-    };
+    }
   },
 };
 </script>

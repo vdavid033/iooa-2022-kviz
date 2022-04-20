dfd7fcfc6922bf85cf57c33df10d7f26a5aea408
vdavid033 <vlatka.davidovic@gmail.com>
2022-04-21 00:18:15 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ var app = express();
 var bodyParser = require('body-parser');
 const dbConfig = require("./db.config.js");
 var mysql = require('mysql');
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:8080'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -20,9 +24,7 @@ dbConn.connect();
 app.get('/plant_species', function (request, response) {
     dbConn.query('SELECT * FROM plant_species', function (error, results, fields) {
         if (error) throw error;
-        return response.send({
-            error: false, data: results, message: 'plant_species_list.'
-        });
+        return response.send(results);
     });
 
     module.exports = app;
@@ -46,7 +48,9 @@ app.get('/plant_species/:id', function (request, response) {
     let plant_species_id = request.params.id;
     if (!plant_species_id) {
         return response.status(400).send({
-            error: true, message: 'Please provide plant_species_id'
+            error: true, 
+            
+            message: 'Please provide plant_species_id'
         });
     }
     dbConn.query('SELECT * FROM plant_species where id=?', plant_species_id, function
@@ -54,7 +58,7 @@ app.get('/plant_species/:id', function (request, response) {
         if (error) throw error;
         return response.send({
             error: false, data: results[0], message:
-                'plant_species list.'
+                'error plant_species list.'
         });
     });
 });

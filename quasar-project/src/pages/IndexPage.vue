87bdda5152052fba0e45aa60f99b049936bf9c82
marija <siladjev.marija98@gmail.com>
2022-04-20 12:42:52 +0200

    Prijedlog rjesenja za prikaz podataka o bot.fam. za plant id

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-px-sm">
+      Odaberi točan croatian_name i latin_name biljne porodice za biljnu vrstu: (prikaz podataka iz tablice plant)  <strong>{{ shape }}</strong>
+    </div>
+    <q-option-group
+      :options="options"
+      type="radio"
+      v-model="group"
+    />
+    <div>
+        <q-btn label="Potvrdi" type="submit" color="green-6"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      group: ref('option1'),
+
+      options: [
+        { label: 'random_croatian_latin_name', value: 'option1', color: 'green' },
+        { label: 'random_croatian_latin_name', value: 'option2', color: 'green' },
+        { label: 'random_croatian_latin_name', value: 'option3', color: 'green'},
+        { label: 'correct_croatian_latin_name', value: 'option4', color: 'green' },
+      ]
+    }
+  }
+}
+</script>

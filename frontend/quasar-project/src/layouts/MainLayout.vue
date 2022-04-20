58d7059a882cbc719ad9790c02c054a8de76e682
tkrznaric <tkrznaric@veleri.hr>
2022-04-20 19:19:19 +0200

    Quasar project

---
+++ frontend/quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-positive text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title class="text-weight-medium text-center text-h3">
+          <q-avatar>
+            <img src="https://images.news18.com/ibnlive/uploads/2021/08/new-mermaid-plant-species-in-andaman-16292679803x2.png">
+          </q-avatar>
+          Dobrodošli u kviz o biljnim vrstama
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+const linksList = [
+  {
+    title: 'Link test',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ frontend/quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="fit row wrap justify-center items-start content-center">
+    <div class="row">
+      <div class="col"> <img alt="Quasar logo" src="https://scx1.b-cdn.net/csz/news/800a/2017/worldsbotani.jpg"></div>
+    </div>
+
+    <div class="row">
+      <div class="col"><q-btn color="positive" label="Pokreni kviz" class="text-h5 text-center" /></div>
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>

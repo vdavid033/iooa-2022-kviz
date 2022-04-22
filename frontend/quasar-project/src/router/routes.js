4d53618537639f614f3a1abc55963891aba82413
vdavid033 <vlatka.davidovic@gmail.com>
2022-04-23 00:45:50 +0200

    ruta do KvizPage.vue

--- frontend/quasar-project/src/router/routes.js
+++ frontend/quasar-project/src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'kviz', component: () => import('pages/KvizPage.vue') }
     ]
   },
 

4750fb657ee7dd4ddfaa4e2fef61fb0e3f437a7c
vdavid033 <vlatka.davidovic@gmail.com>
2022-05-04 11:37:39 +0200

    Dodan axios u konfiguraciju

---
+++ frontend/quasar-project/src/boot/axios.js
@@ -0,0 +1,17 @@
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+// export default boot(async (/* { app, router, ... } */) => {
+//   // something to do
+// })
+export default boot(({ app }) => {
+  // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  app.config.globalProperties.$axios = axios
+  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+  //       so you won't necessarily have to import axios in each vue file
+})
+
+export { axios }
